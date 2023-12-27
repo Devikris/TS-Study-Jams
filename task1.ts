924b7d5c8cb2d4535da7826a870ced2a0ea643e9
Devi Krishna <96735209+Devikris@users.noreply.github.com>
2023-12-27 13:12:48 +0530

    Create task1.ts

---
+++ task1.ts
@@ -0,0 +1,20 @@
+function registerUser(name:string, birthYear:number)
+ {
+    const isUserOfAge = checkAge(birthYear);
+    if (isUserOfAge) 
+    {
+        console.log("User Registration Success!");
+    } else 
+    {
+        console.log("User Registration Failed!");
+    }
+}
+function checkAge(birthYear:number)
+ {
+    const currentYear = new Date().getFullYear();
+    const age = currentYear - birthYear;
+    return age >= 18;
+}
+const userName = "Devi Krishna R";
+const userBirthYear = 2004;
+registerUser(userName, userBirthYear);
